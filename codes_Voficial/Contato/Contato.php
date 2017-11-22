c6ff5914a5336eb3dfeb0efa21221b985c3b30fd
Pedro e Júlia <pedrootavioferri@hotmail.com>
2017-11-22 08:22:30 -0200

    //Pedro e Júlia

--- codes_Voficial/Contato/Contato.php
+++ codes_Voficial/Contato/Contato.php
@@ -1,8 +1,26 @@
 <?php
+include "../cabecalho.php"
+  ?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+  <!DOCTYPE html>
+  <html>
+  <link rel="stylesheet" type="text/css" href="../CSS/contato.css">
+  <head>
+  	<title></title>
+  </head>
+  <body>
+  
+  <div id="contato">
+  	<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
 
+  </div>
+
+<img id="imgAcad" src="../IMAGENS/academia.jpg">
+
+  </body>
+  </html>
