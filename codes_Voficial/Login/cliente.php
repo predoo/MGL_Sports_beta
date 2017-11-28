f469ec255bc7879467dde079a39dfc2b7296a78d
Pedro e Júlia <pedrootavioferri@hotmail.com>
2017-11-28 20:57:54 -0200

    //Júlia, juliano, luana, marcele e pedro

--- codes_Voficial/Login/cliente.php
+++ codes_Voficial/Login/cliente.php
@@ -1,8 +1,24 @@
 <?php
+include "../Cabecalho/Cabecalho.php";
+include "../banco.php";
+$sql = "select resposta from usuario where idUsuario = '".$_GET['email']."'";
+$result = mysqli_query($link, $sql);
+  $row = $result -> fetch_assoc();
+   $resposta = $row['resposta'];
+   
+  
+echo "<!DOCTYPE html>
+ <html>
+ <link rel='stylesheet' type='text/css' href='../CSS/index.css'>
+ <head>
+  <title></title>
+ </head>
+ <body>
+ <div id='a'>
+  <h1>Avaliação do preparador físico: </h1>".$resposta."
+</div>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+</body>
+</html>";
 
+?>
--- codes_Voficial/Login/confirma.php
+++ codes_Voficial/Login/confirma.php
@@ -4,46 +4,40 @@ include '../banco.php';
 $email = $_GET["email"];
 $senha = $_GET["senha"];
 
-if(isset($_GET['opcoes'])){
-    $sql = "select senha from preparador_fisico where email = '".$email."';";
-     $result = mysqli_query($link, $sql);
-$row = $result -> fetch_assoc();
+        if(isset($_GET['opcoes'])){
+             $sql = "select senha from preparador_fisico where email = '".$email."';";
+             $result = mysqli_query($link, $sql);
+             $row = $result -> fetch_assoc();
 
-$senhab = $row['senha'];
-if($senha == $senhab){
-         header('Location: ../Home/Home.php');
+            $senhab = $row['senha'];
+            if($senha == $senhab){
+            header('Location: ../Login/admin.php');
 
-}else{
-    echo "<br><h2 align='center'>Login incorreto do administrador</h2>";
-  echo "<form action='login.php' method='GET' align='center'>";
-  echo "<input type='submit' value='Tentar novamente'>";
-}
+}           else{
+            echo "<br><h2 align='center'>Login incorreto do administrador</h2>";
+            echo "<form action='login.php' method='GET' align='center'>";
+            echo "<input type='submit' value='Tentar novamente'>";
+            }
     
-}else{   
-    $senha2 = md5($senha);
+}       else{   
+        $senha2 = md5($senha);
 
-    $sql = "select senha from usuario where email = '".$email."';";
-    $result = mysqli_query($link, $sql);
-$row = $result -> fetch_assoc();
+         $sql = "select senha from usuario where email = '".$email."';";
+         $result = mysqli_query($link, $sql);
+            $row = $result -> fetch_assoc();
 
-$senhacrip = $row['senha'];
+            $senhacrip = $row['senha'];
 
-if($senha2 == $senhacrip){
- if(isset($_GET['opcoes'])){
-     header('Location: ../Home/Home.php');
-}
-else{
-     header('Location: ../Valores/valores.php');
-}
-
+        if($senha2 == $senhacrip){
+         header('Location: ../Login/cliente.php?email='.$email.'');
 }else{
   echo "<br><h2 align='center'>Login incorreto</h2>";
   echo "<form action='login.php' method='GET' align='center'>";
   echo "<input type='submit' value='Tentar novamente'>";
   
 }
-}
 
+}
 
 
 $link->close();
